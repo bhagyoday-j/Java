88ae14b93e4d6a2f45dc655d8d737957c33c5a86
bhagyoday-j <bmj3112005@gmail.com>
2026-02-20 19:47:32 +0530

    4-packege

--- 4-packeges/database/DBConnection.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-public class DBConnection {
-
-    public void connect() {
-        System.out.println("Database connected successfully");
-        System.out.print("\n");
-    }
-}
